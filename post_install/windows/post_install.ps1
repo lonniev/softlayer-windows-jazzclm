d8fe727bc49cd6f27301a5ba6638ee09b1f8dcd8
Lonnie VanZandt <lonniev@gmail.com>
2014-08-12 15:57:44 -0600

    darn spaces and msdos

--- post_install/windows/post_install.ps1
+++ post_install/windows/post_install.ps1
@@ -51,10 +51,10 @@ C:\tmp\BvSshServer-Inst.exe -acceptEULA -startService -defaultSite
 
 # configure it to sync with users' authorized_keys files
 $cmds = @'
-access.authKeysSync true                                                                                                            
+access.authKeysSync true
 commit
 '@
-echo $cmds | "C:\Program Files\Bitvise SSH Server\BssCfg.exe" settings importText -i
+$cmds | C:\"Program Files"\"Bitvise SSH Server"\BssCfg.exe settings importText -i
 
 # obtain an rsync and chmod client for windows
 # load the assembly required
