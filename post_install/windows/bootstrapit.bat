30c572801aab38cdcbd1ed608f045cbad046f58c
Lonnie VanZandt <lonniev@gmail.com>
2014-08-12 16:56:08 -0600

    see if I can retain a copy of the post_install file

--- post_install/windows/bootstrapit.bat
+++ post_install/windows/bootstrapit.bat
@@ -1,1 +1,1 @@
-powershell -command "& {set-executionpolicy -force unrestricted; iwr https://raw.githubusercontent.com/lonniev/softlayer-windows-jazzclm/master/post_install/windows/post_install.ps1 -OutFile post_install.ps1; .\post_install.ps1}"
+powershell -command "& {set-executionpolicy -force unrestricted; iwr https://raw.githubusercontent.com/lonniev/softlayer-windows-jazzclm/master/post_install/windows/post_install.ps1 -OutFile c:\post_install.ps1; c:\post_install.ps1}"
