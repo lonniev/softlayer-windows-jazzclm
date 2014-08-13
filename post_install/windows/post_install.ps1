e61f4269c1275f1b6ce45c061c559b4753666011
Lonnie VanZandt <lonniev@gmail.com>
2014-08-12 22:46:32 -0600

    remove specifying homedir and setting permissions. name key as vagrant

--- post_install/windows/post_install.ps1
+++ post_install/windows/post_install.ps1
@@ -1,40 +1,3 @@
-# get a reference to the local OS configurator
-$computer = [ADSI]"WinNT://localhost"
-
-# create the vagrant user with password vagrant
-$userName = "vagrant"
-$userHome = "c:\users\" + $userName
-md $userHome
-$user = $computer.Create("User",$userName )
-$user.setpassword( $userName )
-$user.put("description", "Vagrant User")
-$user.put("homedirectory", $userHome)
-$user.put("profile", $userHome)
-$user.SetInfo()
-
-# ADS_UF_DONT_EXPIRE_PASSWD flag is 0x10000
-$user.UserFlags[0] = $user.UserFlags[0] -bor 0x10000
-$user.SetInfo()
-
-# add the users created to be added to the local administrators group.
-net localgroup Administrators /add $userName
-
-# in unix this would be chown -R vagrant.vagrant /home/vagrant
-$account="\\"+$user.Name
-$homedir=$user.HomeDirectory
-$rights=[System.Security.AccessControl.FileSystemRights]::FullControl
-$inheritance=[System.Security.AccessControl.InheritanceFlags]"ContainerInherit,ObjectInherit"
-$propagation=[System.Security.AccessControl.PropagationFlags]::None
-$allowdeny=[System.Security.AccessControl.AccessControlType]::Allow
-
-$dirACE=New-Object System.Security.AccessControl.FileSystemAccessRule ($account,$rights,$inheritance,$propagation,$allowdeny)
-
-$dirACL=Get-Acl $homedir
-
-$dirACL.AddAccessRule($dirACE)
-
-Set-Acl $homedir $dirACL
-
 # configure WinRM
 Set-WSManQuickConfig -Force
 
@@ -43,6 +6,25 @@ Set-Item WSMAN:\LocalHost\Client\AllowUnencrypted -Value $true
 Set-Item WSMAN:\LocalHost\Client\Auth\Basic -Value $true
 
 Set-Service WinRM -startuptype "automatic"
+
+# get a reference to the local OS configurator
+$computer = [ADSI]"WinNT://localhost"
+
+# create the vagrant user with password vagrant
+$userName = "vagrant"
+$userHome = "c:\users\" + $userName
+md $userHome
+$user = $computer.Create("User", $userName )
+$user.setpassword( $userName )
+$user.put("description", "Vagrant User")
+$user.SetInfo()
+
+# ADS_UF_DONT_EXPIRE_PASSWD flag is 0x10000
+$user.UserFlags[0] = $user.UserFlags[0] -bor 0x10000
+$user.SetInfo()
+
+# add the user created to be added to the local administrators group.
+net localgroup Administrators /add $userName
 
 # obtain a sshd server for windows
 md c:\tmp
@@ -69,12 +51,7 @@ setx PATH "$env:path;c:\DeltaCopy" -m
 # copy the vagrant public key to this vagrant user
 $vssh=$userHome + "\.ssh"
 md $vssh\authorized_keys
-c:\DeltaCopy\chmod.exe -R 700 $vssh
-iwr https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -OutFile $vssh\authorized_keys\vagrant.pub
-c:\DeltaCopy\chmod.exe -R 0600 $vssh\authorized_keys
-
-$owner = new-object system.security.principal.ntaccount("vagrant")
-get-childitem -literalpath $vssh -force -recurse | Get-Acl | foreach-object { $_.setOwner($owner) }
+iwr https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -OutFile $vssh\authorized_keys\vagrant
 
 # schedule a restart of the instance
 shutdown /r /t 10 /c "server reboot to complete vagrant post_install" /d p:2:4
