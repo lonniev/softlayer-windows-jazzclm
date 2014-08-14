c1cbce1961fad98d18197ac00e2955bb91d3ad08
Lonnie VanZandt <lonniev@gmail.com>
2014-08-14 15:54:06 -0600

    disable shutdown to further debug

--- post_install/windows/post_install.ps1
+++ post_install/windows/post_install.ps1
@@ -103,4 +103,4 @@ Start-Sleep 10
 Write-Host -ForegroundColor Green "Done. Bye..."
 Write-Progress -Activity "Vagrant Post Install" -Status "Scheduling restart..." -PercentComplete 90 -SecondsRemaining 5
 
-shutdown -r -t 5 -c "server reboot to complete vagrant post_install." -d p:2:4
+#shutdown -r -t 5 -c "server reboot to complete vagrant post_install." -d p:2:4
